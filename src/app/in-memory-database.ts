18435eeed0c77d0fb2685ae61eea10568d85e459
Lucas Souza <lucaseduardoid1@gmail.com>
2020-01-13 21:06:35 -0400

    improving category list component

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -8,7 +8,7 @@ export class InMemoryDatabase implements InMemoryDbService{
             {id: 1, name: 'Moradia', description: 'Pagamentos de Contas de Casa'},
             {id: 2, name: 'Saúde', description: 'Plano de Saúde e Remédios'},
             {id: 3, name: 'Lazer', description: 'Cinema, parques, praia, etc.'},
-            {id: 4, name: 'Salaŕio', description: 'Recebimento de Salário'}
+            {id: 4, name: 'Salário', description: 'Recebimento de Salário'}
         ];
 
         return { categories }
